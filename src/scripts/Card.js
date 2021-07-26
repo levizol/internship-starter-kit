cda636af04b2fcee582af54782fea195332838be
ionutganea24 <ionutganea24@gmail.com>
2021-07-26 10:01:10 +0300

    Last commit

--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -1,4 +1,13 @@
+/**
+ * Class representing a card
+ */
 class Card {
+  /**
+   * @param {String} id
+   * @param {String} description
+   * @param {String} label
+   * @param {String} time
+   */
   constructor(id, description, label, time) {
     this.id = id;
     this.description = description;
